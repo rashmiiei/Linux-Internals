2b0cfc8cd8ff45bc0216eb50be0e86088253370b
Rashmi <reach.rashmi9@gmail.com>
2026-05-06 22:38:25 +0530

    Add files via upload

---
+++ process/fork_switch_example.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Print a message before fork() is called
+    printf("Before fork() \n");
+    
+    // fork() creates a new process
+    // pid will be:
+    //   -1 if fork failed
+    //    0 in the child process
+    //   >0 (child PID) in the parent process
+    int pid = fork();
+
+
+    // Use switch-case to handle the three possible outcomes of fork()
+    switch(pid)
+    {
+        case -1:
+            // Error case: fork failed
+            perror("fork");
+            exit(1);
+        case 0:
+            // Child process branch
+            printf("This is child process \n");
+            sleep(10);
+            printf("Child terminated \n");
+            exit(0);
+        default:
+            // Parent process branch
+            printf("This is parent process \n");
+            sleep(10);
+            printf("Parent terminated \n");
+            exit(0);
+    }
+
+    return 0;
+}
